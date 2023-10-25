7547892633816e868b3ef9efc4784275dfab060a
Geoffrey Gane <geoffrey.gane@wttj.co>
2023-10-25 11:00:02 +0200

    feat: add chap 12 : Modeling with structs

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,73 +1,86 @@
 defmodule Servy.Handler do
+  @moduledoc "Handles HTTP requests."
+
+  import Servy.FileHandler, only: [handle_file: 2]
+  import Servy.Parser, only: [parse: 1]
+  import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
+
+  alias Servy.Conv
+
+  @pages_path Path.expand("../../pages", __DIR__)
+
+  @doc "Transforms the request into a response"
   def handle(request) do
     request
     |> parse
+    |> rewrite_path
     |> log
     |> route
+    |> track
+    |> emojify
     |> format_response
   end
 
-  def log(conv), do: IO.inspect conv
-
-  def parse(request) do
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first()
-      |> String.split(" ")
-
-    %{
-      method: method,
-      path: path,
-      resp_body: "",
-      status: nil
-    }
+  def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
+    %Conv{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(conv) do
-    route(conv, conv.method, conv.path)
+  def route(%Conv{method: "GET", path: "/bears"} = conv) do
+    %Conv{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
-  def route(conv, "GET", "/wildthings") do
-    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
+  def route(%Conv{method: "GET", path: "/bears" <> id} = conv) do
+    %Conv{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
-  def route(conv, "GET", "/bears") do
-    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
+  def route(%Conv{method: "DELETE", path: "/bears/" <> _id} = conv) do
+    %Conv{conv | status: 403, resp_body: "Bears must never be deleted!"}
   end
 
-  def route(conv, "GET", "/bears/" <> id) do
-    %{conv | status: 200, resp_body: "Bear #{id}"}
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
+    @pages_path
+    |> Path.join("about.html")
+    |> File.read()
+    |> handle_file(conv)
   end
 
-  def route(conv, "DELETE", "/bears/" <> id) do
-    %{conv | status: 200, resp_body: "Bear #{id} no longer exists"}
+  def route(%Conv{method: "GET", path: "/bears/new"} = conv) do
+    @pages_path
+    |> Path.join("form.html")
+    |> File.read()
+    |> handle_file(conv)
   end
 
-  def route(conv, _method, path) do
+  def route(%Conv{method: "GET", path: "/pages/" <> page} = conv) do
+    @pages_path
+    |> Path.join(page <> ".html")
+    |> File.read()
+    |> handle_file(conv)
+  end
+
+  def route(%Conv{path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
-  def format_response(conv) do
+  defp emojify(%Conv{status: 200} = conv) do
+    emojies = String.duplicate("🎉", 5)
+    body = emojies <> "\n" <> conv.resp_body <> "\n" <> emojies
+
+    %{conv | resp_body: body}
+  end
+
+  defp emojify(%Conv{} = conv), do: conv
+
+  def format_response(%Conv{} = conv) do
     """
-    HTTP.1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP.1.1 #{Conv.full_status(conv)}
     Content-type: text/html
-    Content-length: #{String.length(conv.resp_body)}
+    Content-length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
   end
 
-  defp status_reason(code) do
-    %{
-      200 => "OK",
-      201 => "Created",
-      401 => "Unauthorized",
-      403 => "Forbidden",
-      404 => "Not Found",
-      500 => "Internal Server Error",
-    }[code]
-  end
 end
 
 request = """
@@ -130,3 +143,86 @@ response = Servy.Handler.handle(request)
 
 IO.puts(response)
 
+request = """
+GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /bears?id=1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /bears/new HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /pages/contact HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /pages/faq HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /pages/any-other-page HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
---
+++ lib/servy/plugin.ex
@@ -0,0 +1,27 @@
+defmodule Servy.Plugins do
+  @moduledoc "Handles plugins."
+
+  alias Servy.Conv
+
+  require Logger
+
+  @doc "Logs 404 requests"
+  def track(%Conv{status: 404, path: path} = conv) do
+    Logger.warn("#{path} is on the loose!")
+    conv
+  end
+
+  def track(%Conv{} = conv), do: conv
+
+  def rewrite_path(%Conv{path: "/bears?id=" <> id_number} = conv) do
+    %{conv | path: "/bears/#{id_number}"}
+  end
+
+  def rewrite_path(%Conv{path: "/wildlife"} = conv) do
+    %{conv | path: "/wildthings"}
+  end
+
+  def rewrite_path(%Conv{} = conv), do: conv
+
+  def log(%Conv{} = conv), do: IO.inspect(conv)
+end
